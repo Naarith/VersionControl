d8f653ad6d74da6c80401d3550b2b9615c0485ee
AlanLM <a_rabbit93@yahoo.com>
2016-03-18 19:26:51 -0700

    erase main
    made a blank main

--- VersionControl/src/Repository.java
+++ VersionControl/src/Repository.java
@@ -2,9 +2,6 @@ public class Repository
 {
 	public static void main(String[] args)
 	{
-		String test = "I don't understand Git!" ; 
 		
-		System.out.println(test) ; 
-		System.out.println("Trying to make another branch!");
 	} // end of main 
 } // end of Repository Project
