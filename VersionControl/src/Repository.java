9f6311ca9ca469ee27552ff3e327b5f55ac57cab
alanlm93 <a_rabbit93@yahoo.com>
2016-03-18 18:19:30 -0700

    testing commits

--- VersionControl/src/Repository.java
+++ VersionControl/src/Repository.java
@@ -2,6 +2,8 @@ public class Repository
 {
 	public static void main(String[] args)
 	{
+		String test = "I don't understand Git!" ; 
 		
+		System.out.println(test) ; 
 	} // end of main 
 } // end of Repository Project
