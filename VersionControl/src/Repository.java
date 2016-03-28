59bb34afe35dd0a457505f3ca1f37af846d51191
Narith Choeun <narithchoeun@hotmail.com>
2016-03-27 19:18:53 -0700

    created command line argument options

--- VersionControl/src/Repository.java
+++ VersionControl/src/Repository.java
@@ -15,7 +15,8 @@ public class Repository
 {
 	public static void main(String[] args)
 	{
-		Repository repo = new Repository(get_source());	
+		Repository repo = new Repository(args[0]);	
+		repo.set_target(args[1]);
 		repo.create_repo();
 	} // end of main
 	
@@ -38,8 +39,6 @@ public class Repository
 	 * Creates a repository for a new or existing file
 	 */
 	public void create_repo(){
-		tgt_file = get_target();
-		
 		boolean created = tgt_file.mkdir();
 		if(created) 
 			System.out.println("Repository created.");
@@ -53,43 +52,19 @@ public class Repository
 	}
 	
 	/**
-	 * Gets the file path from the source file 
-	 * @return source file path 
+	 * Sets target folder based on user argument
 	 */
-	public static String get_source(){
-		System.out.println("Select the pathname for a source folder");
-//		String source = in.nextLine();
-//		String source = "/Users/narithchoeun/Desktop/source"; //mac
-//		String source = "E:\\Desktop\\source";
-
-		String source = "/Users/Alan/Desktop/test_project" ; // Alan's computer
-		return source;
+	public void set_target(String s){
+		tgt_file = new File(s+"/repo343");
 	}
-	
-	/**
-	 * Getting the target folder specified by the user 
-	 * @return target file 
-	 */
-	public File get_target(){
-		System.out.println("Select the pathname a target folder");
-//		String pathname = in.nextLine();
-//		String pathname = "/Users/narithchoeun/Desktop"; //mac
-//		String pathname = "E:\\Desktop\\"; //windows
-		String pathname = "/Users/Alan/Desktop" ; // Alan's computer 
-//		pathname += "\\repo343";
-		pathname += "/repo343";//mac
-		
-		File target_dir = new File(pathname);
-		return target_dir;
-	} // end of get_target method 
-	
+
 	/**
 	 * Copying the source file(s) into a specified target 
 	 * @param source File to be copied 
 	 * @param target File copied from source 
 	 */
 	public void copy_source(File source, File target){
-		System.out.println("Source file: " + source.getPath() + " is being copied.");
+		System.out.println("Source file: " + source.getPath() + " is being copied.\n");
 		//creates project tree folder
 		File ptree_dir = new File(target+"/"+source.getName());
 		ptree_dir.mkdir();
@@ -101,14 +76,10 @@ public class Repository
 				in = new Scanner(select_file); //read the file
 				
 				//file path to create directories that contains the source file's artifacts
-//				File temp_dir = new File("\\"+ptree_dir.getPath()+"\\"+select_file.getName());
-//				File temp_dir = new File("/"+ptree_dir.getPath()+"/"+select_file.getName()); //mac
 				File temp_dir = new File(ptree_dir.getPath()+"/"+select_file.getName()) ; 
 				temp_dir.mkdir();
 				
 				//write into the created directory with an artifact of the file
-//				File write_file = new File("\\"+temp_dir.getPath()+"\\"+select_file.getName());
-//				File write_file = new File("/"+temp_dir.getPath()+"/"+checksum(select_file)+get_extension(select_file));//mac
 				File write_file = new File(temp_dir.getPath()+"/"+checksum(select_file)+get_extension(select_file)) ;
 				out = new PrintWriter(write_file);
 				
@@ -129,9 +100,7 @@ public class Repository
 	 */
 	public void create_manifest()
 	{
-
-//		String path = tgt_file.getPath() + "\\manifest" ;
-		String path = tgt_file.getPath() + "/manifest"; //mac
+		String path = tgt_file.getPath() + "/manifest"; 
 
 		File manifest = new File(path) ; 
 		
@@ -139,14 +108,12 @@ public class Repository
 		String time = get_timestamp();
 
 		//creates man line file with check in timestamp and the project hierarchy
-//		File man_line = new File("/"+manifest.getPath()+"/"+time+".txt");
 		File man_line = new File(manifest.getPath()+"/"+time+".txt") ; // Alan's comp
 		try{
 			out = new PrintWriter(man_line);
 			out.println("Manifest-Version 1.0 \n" + "Created on: " + time);
 			for(File select_file : src_file.listFiles()){
-//				out.println(src_file.getPath()+"/"+select_file.getName()); //mac
-				out.println(src_file.getPath()+"\\"+select_file.getName()); // Alan's comp
+				out.println(src_file.getPath()+"/"+checksum(select_file)+get_extension(select_file)); //mac
 			}
 			out.flush();
 		} catch (IOException e) { e.printStackTrace(); }
